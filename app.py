4dbf4e1fec0219c0763dfe84789f71e177ac7f41
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-03-22 18:00:15 +0200

    Add rudimentary login functionality

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
+from werkzeug.utils import redirect
+
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
@@ -15,3 +18,11 @@ def index():
     result = db.session.execute("SELECT introduction FROM pages WHERE id=1")
     introductory_text = result.fetchone()[0]
     return render_template("index.html", name=name, introductory_text=introductory_text)
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    #MISSING: username & password check
+    session["username"] = username
+    return redirect("/")
