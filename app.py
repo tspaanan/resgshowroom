834b84009281ff434d6fbd839d38c375c603f90d
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-03-25 10:48:43 +0200

    PI can add and change subpages

--- app.py
+++ app.py
@@ -18,12 +18,16 @@ def index():
     name = result.fetchone()[0]
     result = db.session.execute("SELECT introduction FROM pages WHERE id=1")
     introductory_text = result.fetchone()[0]
-    #tarkistetaan oikeudet: PI voi muuttaa nimeä
+    
+    #checking credentials: PI can change everything
     allow_pi = False
     if check_credentials.is_pi(db, session):
         allow_pi = True
+
+    result = db.session.execute("SELECT id FROM pages WHERE id>2")
+    subpages_id = result.fetchall()
     return render_template("index.html", name=name, introductory_text=introductory_text, \
-                            allow_pi=allow_pi)
+                            allow_pi=allow_pi, subpages_id=subpages_id)
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -57,32 +61,47 @@ def logout():
 @app.route("/change_text", methods=["POST"])
 def change_text():
     if "change_name" in request.form:
+        print("change_name haara valittu")
         allow_pi = check_credentials.is_pi(db, session)
-        return render_template("change_text.html", allow_pi=allow_pi, form="change_name")
+        page_id = request.form["page_id"]
+        print(page_id)
+        return render_template("change_text.html", allow_pi=allow_pi, form="change_name", page_id=page_id)
     elif "change_introduction" in request.form:
         allow_pi = check_credentials.is_pi(db, session)
-        return render_template("change_text.html", allow_pi=allow_pi, form="change_introduction")
+        page_id = request.form["page_id"]
+        return render_template("change_text.html", allow_pi=allow_pi, form="change_introduction", page_id=page_id)
 
 @app.route("/update", methods=["POST"])
 def update():
-    #TODO: credentials checking before updating
+    #TODO: credentials checking before updating, everytime!!!
     if "changed_name" in request.form:
         new_title = request.form["changed_name"]
-        sql = "UPDATE pages SET title=:new_title WHERE id=1"
-        db.session.execute(sql, {"new_title":new_title})
+        page_id = request.form["page_id"]
+        sql = "UPDATE pages SET title=:new_title WHERE id=:page_id"
+        db.session.execute(sql, {"new_title":new_title, "page_id":page_id})
         db.session.commit()
-        return redirect("/")
+        if page_id == 1:
+            return redirect("/")
+        else:
+            return redirect("member_page/" + str(page_id))
     elif "changed_introduction" in request.form:
         new_introduction = request.form["changed_introduction"]
-        sql = "UPDATE pages set introduction=:new_introduction WHERE id=1"
-        db.session.execute(sql, {"new_introduction":new_introduction})
+        page_id = request.form["page_id"]
+        sql = "UPDATE pages SET introduction=:new_introduction WHERE id=:page_id"
+        db.session.execute(sql, {"new_introduction":new_introduction, "page_id":page_id})
         db.session.commit()
+        if page_id == 1:
+            return redirect("/")
+        else:
+            return redirect("member_page/" + str(page_id))
         return redirect("/")
     elif "new_name" in request.form:
         new_name = request.form["new_name"]
-        sql = "INSERT INTO pages (title) VALUES (:new_name)"
-        db.session.execute(sql, {"new_name":new_name})
+        new_introduction = request.form["new_introduction"]
+        sql = "INSERT INTO pages (title,introduction) VALUES (:new_name,:new_introduction)"
+        db.session.execute(sql, {"new_name":new_name,"new_introduction":new_introduction})
         db.session.commit()
+
         #TODO: redirect to newly created page instead of root
         return redirect("/")
 
@@ -91,3 +110,20 @@ def new_page():
     #TODO: credentials checking before access
     #if personal page already exists, redirect there
     return render_template("new_page.html")
+
+@app.route("/member_page/<int:page_id>")
+def member_page(page_id):
+    sql = "SELECT title FROM pages WHERE id=:page_id"
+    result = db.session.execute(sql, {"page_id":page_id})
+    name = result.fetchone()[0]
+    sql = "SELECT introduction FROM pages WHERE id=:page_id"
+    result = db.session.execute(sql, {"page_id":page_id})
+    introductory_text = result.fetchone()[0]
+    
+    #checking credentials: PI can change everything
+    allow_pi = False
+    if check_credentials.is_pi(db, session):
+        allow_pi = True
+
+    return render_template("member_page.html", name=name, introductory_text=introductory_text, \
+                            allow_pi=allow_pi, page_id=page_id)
