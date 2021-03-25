814b57c013a85cda7739201c7f1e8d452868fa75
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-03-25 16:05:05 +0200

    Update SQL-schema

--- schema.sql
+++ schema.sql
@@ -10,3 +10,9 @@ CREATE TABLE pages (
 	title TEXT,
 	introduction TEXT
 );
+
+CREATE TABLE page_ownership (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	page_id INTEGER REFERENCES pages
+);
