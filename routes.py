8887b272c1793ee4c3837b40762e27da9fc405b3
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-05-07 11:38:51 +0300

    Refactored routes.py for more readability

--- routes.py
+++ routes.py
@@ -50,36 +50,50 @@ def index():
 def archive_message():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
     if check_credentials.is_pi():
         message_id = request.form["message_id"]
         sql_quories.archive_message(message_id)
         return redirect("/view_feedback")
+    
     else: return render_template("error", error="insufficient credentials")
 
 @app.route("/change_text", methods=["POST"])
 def change_text():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
+    page_id = request.form["page_id"]
+    if not check_credentials.check_page_ownership(page_id):
+        return render_template("error.html", error="insufficient credentials")
+    
     allow_pi = check_credentials.is_pi()
     allow_member = check_credentials.is_member()
-    page_id = request.form["page_id"]
     if "old_text" in request.form:
         old_text = request.form["old_text"]
+    
     if "change_name" in request.form:
         return render_template("change_text.html", allow_pi=allow_pi, allow_member=allow_member, form="change_name", page_id=page_id, old_text=old_text)
+    
     elif "change_introduction" in request.form:
         return render_template("change_text.html", allow_pi=allow_pi, allow_member=allow_member, form="change_introduction", page_id=page_id, old_text=old_text)
+    
     elif "add_new_keyword" in request.form:
         return render_template("change_text.html", allow_pi=allow_pi, allow_member=allow_member, form="add_new_keyword", page_id=page_id)
+    
     elif "add_new_publication" in request.form:
         return render_template("change_text.html", allow_pi=allow_pi, allow_member=allow_member, form="add_new_publication", page_id=page_id)
+
+    else: return redirect("/")
 
 @app.route("/delete_logo", methods=["POST"])
 def delete_logo():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
     if not check_credentials.check_page_ownership(1):
         return render_template("error.html", error="insufficient credentials")
+    
     allow_pi = check_credentials.is_pi()
     logo_ids = sql_quories.fetch_image_ids()
     logos = sql_quories.fetch_images()
@@ -90,25 +104,32 @@ def delete_logo():
 def delete_text():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
     page_id = request.form["page_id"]
     if not check_credentials.check_page_ownership(page_id):
         return render_template("error.html", error="insufficient credentials")
+    
     allow_pi = check_credentials.is_pi()
     allow_member = check_credentials.is_member()
+    
     if "delete_publication" in request.form:
         publications_all = sql_quories.fetch_publications(page_id)
         publications = __strip_None_values(publications_all)
         return render_template("delete_text.html", publications=publications, allow_pi=allow_pi, allow_member=allow_member,
                                 form="delete_publication", page_id=page_id)
+    
     elif "delete_keyword" in request.form:
         keywords = sql_quories.fetch_keywords(page_id)
         return render_template("delete_text.html", keywords=keywords, allow_pi=allow_pi, allow_member=allow_member,
                                 form="delete_keyword", page_id=page_id)
 
+    else: return redirect("/")
+    
 @app.route("/download", methods=["POST"])
 def download():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
     topic_id = request.form["topic_id"]
     if check_credentials.check_topic_ownership(topic_id):
         document_id = request.form["document_id"]
@@ -117,6 +138,7 @@ def download():
         response.headers.set("Content-Type","application/msword")
         response.headers.set("Content-Disposition","attachment; filename=" + fetched_document[0][0])
         return response
+
     else: return render_template("error.html", error="insufficient credentials")
 
 @app.route("/login", methods=["POST"])
@@ -124,6 +146,7 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
     user_password = sql_quories.fetch_password(username)
+
     if user_password == None:
         return render_template("error.html", error="no_user")
     else:
@@ -163,6 +186,7 @@ def new_message():
         sql_quories.insert_message(1, content, 0, session)
         session["feedback_left"] = True
         return redirect("/")
+
     elif "new_comment" in request.form:
         content = request.form["new_comment"]
         topic_id = request.form["topic_id"]
@@ -171,28 +195,36 @@ def new_message():
         sql_quories.insert_message(0, content, topic_id, session)
         return redirect("/student_topics/" + str(topic_id))
         
+    else: return redirect("/")
+
 @app.route("/new_page", methods=["POST"])
 def new_page():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+
     if check_credentials.is_member() and check_credentials.check_page_ownership(0):
         return render_template("error.html", error="already has personal page")
+    
     if check_credentials.is_pi() or check_credentials.is_member():
         return render_template("new_page.html")
+    
     else: return render_template("error.html", error="insufficient credentials")
 
 @app.route("/new_topic", methods=["POST"])
 def new_topic():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+
     if check_credentials.is_pi() or check_credentials.is_member():
         return render_template("new_topic.html")
+
     else: return render_template("error.html", error="insufficient credentials")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
+
     else:
         new_password = request.form["new_password"]
         repeat_password = request.form["repeat_password"]
@@ -212,8 +244,10 @@ def register():
         
         if new_username == "":
             return render_template("error.html", error="no username set")
+
         if check_credentials.check_username(new_username):
             return render_template("error.html", error="username exists already")
+
         hashed_password = generate_password_hash(new_password)
         sql_quories.insert_user(new_username, hashed_password, "student")
         session["username"] = new_username
@@ -225,9 +259,11 @@ def register():
 def reserve_topic():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+
     if check_credentials.is_student():
         sql_quories.reserve_topic(session, request.form["topic_id"])
         return redirect("/student_topics/" + request.form["topic_id"])
+
     else: return render_template("error.html", error="insufficient credentials")
 
 @app.route("/student_topics/<int:page_id>")
@@ -240,6 +276,7 @@ def student_topics(page_id):
     own_topic = False
     messages = ()
     latest_document_id = None
+
     if page_id != 0:
         topic_content = sql_quories.fetch_topic_content(page_id)
         if check_credentials.check_topic_ownership(page_id):
@@ -248,7 +285,7 @@ def student_topics(page_id):
             latest_document_id = sql_quories.fetch_latest_document_id(page_id)
             if latest_document_id != None:
                 latest_document_id = latest_document_id[0]
-            print(latest_document_id)
+
     return render_template("student_topics.html", allow_pi=allow_pi,
                         allow_member=allow_member, allow_student=allow_student, topic_ids=topic_ids, page_id=page_id, \
                         topic_content=topic_content, own_topic=own_topic, messages=messages, latest_document_id=latest_document_id)
@@ -257,6 +294,7 @@ def student_topics(page_id):
 def update():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+
     if "changed_name" in request.form:
         new_title = request.form["changed_name"]
         page_id = request.form["page_id"]
@@ -267,6 +305,7 @@ def update():
             if page_id == "1": return redirect("/")
             else: return redirect("member_page/" + str(page_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "changed_introduction" in request.form:
         new_introduction = request.form["changed_introduction"]
         page_id = request.form["page_id"]
@@ -277,6 +316,7 @@ def update():
             if page_id == "1": return redirect("/")
             else: return redirect("member_page/" + str(page_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "new_name" in request.form:
         if check_credentials.is_pi() or check_credentials.is_member():
             new_name = request.form["new_name"]
@@ -288,6 +328,7 @@ def update():
             new_page_id = sql_quories.insert_credentials(session, new_name)
             return redirect("member_page/" + str(new_page_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "new_topic" in request.form:
         if check_credentials.is_pi() or check_credentials.is_member():
             new_topic = request.form["new_topic"]
@@ -297,6 +338,7 @@ def update():
             new_topic_id = sql_quories.insert_topic(new_topic, new_description, session["username"])
             return redirect("student_topics/" + str(new_topic_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "new_keyword" in request.form:
         new_keyword = request.form["new_keyword"]
         page_id = request.form["page_id"]
@@ -307,6 +349,7 @@ def update():
             if page_id == "1": return redirect("/")
             else: return redirect("member_page/" + str(page_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "publication_title" in request.form:
         page_id = request.form["page_id"]
         data_fields = []
@@ -325,6 +368,7 @@ def update():
             if page_id == "1": return redirect("/")
             else: return redirect("member_page/" + str(page_id))
         else: return render_template("error.html", error="insufficient credentials")
+
     elif "delete_publication" in request.form:
         page_id = request.form["page_id"]
         publications = sql_quories.fetch_publications(page_id)
@@ -332,6 +376,7 @@ def update():
             if publication[0] in request.form:
                 sql_quories.remove_publication(publication[0])
         return redirect("/")
+
     elif "delete_keyword" in request.form:
         page_id = request.form["page_id"]
         keywords = sql_quories.fetch_keywords(page_id)
@@ -339,6 +384,7 @@ def update():
             if keyword[0] in request.form:
                 sql_quories.remove_keyword(keyword[0])
         return redirect("/")
+
     elif "delete_logo" in request.form:
         logo_ids = sql_quories.fetch_image_ids()
         for logo_id in logo_ids:
@@ -346,10 +392,13 @@ def update():
                 sql_quories.remove_logo(logo_id[0])
         return redirect("/")
 
+    else: return redirect("/")
+
 @app.route("/upload", methods=["POST"])
 def upload():
     if not check_credentials.csrf_check(request.form["csrf_token"]):
         return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+    
     if "upload_document" in request.files:
         topic_id = request.form["topic_id"]
         if check_credentials.check_topic_ownership(topic_id):
@@ -362,6 +411,7 @@ def upload():
                 sql_quories.insert_file(document_filename, document_data, topic_id, session["username"])
                 return redirect("/student_topics/" + str(topic_id))
             else: return render_template("error.html", error="only doc files are allowed")
+    
     elif "upload_logo" in request.files:
         if check_credentials.is_pi():
             logo_file = request.files["upload_logo"]
@@ -374,6 +424,7 @@ def upload():
                 return redirect("/")
             else: return render_template("error.html", error="only jpg and jpeg files are allowed")
         else: return render_template("error.html", error="insufficient credentials")
+
     else: return render_template("error.html", error="insufficient credentials")
 
 @app.route("/view_feedback", methods=["GET", "POST"]) #archive_message redirects here with GET
@@ -381,10 +432,12 @@ def view_feedback():
     if request.method == "POST":
         if not check_credentials.csrf_check(request.form["csrf_token"]):
             return render_template("error.html", error="detected csrf_vulnerability exploitation attempt")
+
     allow_pi = check_credentials.is_pi()
     archived = "view_archived_feedback" in request.form
     if allow_pi:
         messages = sql_quories.fetch_feedback(archived)
         return render_template("feedback.html", allow_pi=allow_pi, messages=messages,
                                 archived=archived)
+
     else: return render_template("error.html", error="insufficient credentials")
